4aab05f8a4e819d071b793d6bec1375a8bf22780
Elijah Charles <elij@lypy.com>
2026-07-09 06:12:55 +0100

    chore: moved allow list into tool
    - allows sandbox to stay in pure scope by default

--- skills/scripts/eval_lisp.el
+++ skills/scripts/eval_lisp.el
@@ -9,7 +9,20 @@
                                      (error nil)))
                        (result (if expression
                                    (condition-case err
-                                       (let ((res (macher-agent-sandbox-run expression '(message))))
+                                       (let ((res (macher-agent-sandbox-run expression '(cons list string-to-list substring emacs-version
+                                                                                              expt string-upcase string= string< random format
+                                                                                              string-match string-to-number number-to-string
+                                                                                              int-to-string make-vector vector make-list append
+                                                                                              not identity reverse nreverse sort delete delq nconc
+                                                                                              assoc assq rassoc rassq copy-sequence split-string
+                                                                                              replace-regexp-in-string upcase downcase capitalize
+                                                                                              plist-get plist-put plist-member error signal
+                                                                                              make-symbol intern intern-soft match-string
+                                                                                              match-beginning match-end make-string string
+                                                                                              char-to-string aset make-hash-table puthash gethash
+                                                                                              remhash sin cos tan asin acos atan cl-reduce
+                                                                                              cl-position car-safe cdr-safe atom evenp oddp
+                                                                                              message))))
                                          (format "%S" res))
                                      (error (error-message-string err)))
                                  "ERROR: Failed to parse code.")))
